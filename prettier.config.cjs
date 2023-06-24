fc79853b008906af7cbba2594b71012e9549baa8
Thien Nguyen <nvktvan@gmail.com>
2023-06-24 00:30:12 -0700

    Yohoho

---
+++ prettier.config.cjs
@@ -0,0 +1,38 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  semi: false,
+  tabWidth: 2,
+  printWidth: 100,
+  endOfLine: 'lf',
+  singleQuote: true,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  quoteProps: 'consistent',
+
+  importOrder: [
+    '^(react/(.*)$)|^(react$)',
+    '^(next/(.*)$)|^(next$)',
+    '<THIRD_PARTY_MODULES>',
+    '',
+    '^types$',
+    '^@/types/(.*)$',
+    '^@/config/(.*)$',
+    '^@/lib/(.*)$',
+    '^@/hooks/(.*)$',
+    '^@/stores/(.*)$',
+    '^@/components/ui/(.*)$',
+    '^@/components/(.*)$',
+    '^@/styles/(.*)$',
+    '^@/app/(.*)$',
+    '',
+    '^[./]',
+  ],
+
+  importOrderSeparation: false,
+  importOrderSortSpecifiers: true,
+  importOrderBuiltinModulesToTop: true,
+  importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
+  importOrderMergeDuplicateImports: true,
+  importOrderCombineTypeAndValueImports: true,
+  plugins: ['@ianvs/prettier-plugin-sort-imports', 'prettier-plugin-tailwindcss'],
+}
