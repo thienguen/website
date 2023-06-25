8660376fdbc5c93cf477ae0d5ef1e8006a4ca156
Thien Nguyen <nvktvan@gmail.com>
2023-06-25 01:42:23 -0700

    ✅Design: Home page with some stupid quote ( ˙꒳​˙ )

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,36 +3,38 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <>
-      <div
-        /* This use to control background theme, I really don't know how useThemes works so I gave up modify it */
-        className="mx-auto max-w-[1235px] flex-1"
-      >
-        <div className="mt-20 flex w-full flex-col items-center justify-center px-5 sm:mt-40 md:flex-row">
-          <div>
-            {/* <-- This affect everythign, lmao */}
-            <div>
-              <Image
-                src="/images/monika.jpg"
-                alt="Definitely not me!!"
-                width={200}
-                height={200}
-                className="mx-auto rounded-2xl"
-              ></Image>
-            </div>
+      <main className="h-full w-full">
+        <div className="mx-auto mt-20 flex w-full max-w-screen-lg flex-col items-center justify-center px-5 sm:mt-40 sm:flex-row">
+          {/* Left */}
+          <div className="mr-0 flex flex-col justify-center sm:mr-10">
             {/* Title */}
-            <h2 className="">
-              <span>Thien Van Ky Nguyen</span>
+            <h2 className="text-4xl font-bold text-slate-800 dark:text-slate-200 sm:text-5xl md:text-5xl lg:text-6xl">
+              {`Thienguen`}
             </h2>
             {/* Description */}
-            <p>
-              A person who love story rich RPG-Maker games, and want to make one. Often time coding and now we are here.
-              Trust me I am a programmer. C/C++, Java, Kotlin
-            </p>
+            <div className="max-w-md space-y-3 py-5 text-gray-500">
+              <p>A person who loves story-rich RPG-Maker games.</p>
+              <p>Often time coding. Sometimes drawing.</p>
+              <p>Always learning.</p>
+            </div>
+            <p className="font-signature text-2xl">Thien Nguyen</p>
+          </div>
+          {/* Center Bottom */}
+          {/* TODO: kbar will be here */}
 
-            {/* TODO: kbar will be here */}
+          {/* Right */}
+          {/* Profile image */}
+          <div className="">
+            <Image
+              src="/images/monika.jpg"
+              alt="Definitely not me!!"
+              width={150}
+              height={150}
+              className=" mx-auto w-full rounded-3xl"
+            />
           </div>
         </div>
-      </div>
+      </main>
     </>
   )
 }
