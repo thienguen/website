da4e2995dad1eac08fbd8281ed3a62286b218ca6
Thien Nguyen <nvktvan@gmail.com>
2023-07-02 02:42:31 -0700

    Actuallyu worked

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,4 +3,3 @@ import { authOptions } from '@/lib/nextauth/auth'
 
 const handler = NextAuth(authOptions)
 export { handler as GET, handler as POST }
-// export const { auth } = NextAuth(authOptions) // whatever this is for
