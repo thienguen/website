7b68a718e0039b6a4d36415e6d270b9b163e0112
Thien Nguyen <nvktvan@gmail.com>
2023-07-15 17:11:45 -0700

    ♻️ Design: A bit of shitty, will updte and finalize this tonight

--- src/app/guestbook/page.tsx
+++ src/app/guestbook/page.tsx
@@ -37,6 +37,12 @@ export default function Contact() {
 
   const { register, handleSubmit, reset } = useForm<formSchema>()
 
+  const customHandleSubmit =
+    (submitFunction: (data: formSchema) => Promise<void>) => (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault()
+      void handleSubmit((data: FieldValues) => submitFunction(data as formSchema))(e)
+    }
+
   const onSubmit = async (data: formSchema) => {
     console.log(data)
     await createGuestbookEntry({
@@ -47,15 +53,9 @@ export default function Contact() {
     reset()
   }
 
-  const customHandleSubmit =
-    (submitFunction: (data: formSchema) => Promise<void>) => (e: FormEvent<HTMLFormElement>) => {
-      e.preventDefault()
-      void handleSubmit((data: FieldValues) => submitFunction(data as formSchema))(e)
-    }
-
   return (
     <>
-      <div className="mx-auto mb-16 flex max-w-2xl flex-col items-start justify-center">
+      <div className="mx-auto mb-16 mt-8 flex max-w-3xl flex-col items-start justify-center">
         {/* Header */}
         <div className="space-y-2 p-6 md:space-y-5">
           <h1 className="text-xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-2xl sm:leading-10 md:text-3xl">
@@ -68,39 +68,47 @@ export default function Contact() {
 
         {/* SEPARATING FROM HERE */}
 
-        <h1 className="mb-10 flex w-full flex-col items-center justify-center text-center text-xl font-bold tracking-tighter">
-          sign my guestbook
-        </h1>
-      </div>
-      {session?.user ? (
-        <>
-          <form onSubmit={customHandleSubmit(onSubmit)} className="relative max-w-[500px] text-sm">
-            {/* <label htmlFor="content" className="sr-only"> */}
-            {/* Your Message */}
-            {/* </label> */}
-            <input
-              {...register('content', { required: true })}
-              aria-label="Your message"
-              placeholder="Your message..."
-              id="content"
-              name="entry"
-              type="text"
-              required
-              className="mt-1 block w-full rounded-md border-neutral-300 bg-gray-100 py-2 pl-4 pr-32 text-sm text-neutral-900 focus:border-blue-500 focus:ring-blue-500 dark:bg-neutral-800 dark:text-neutral-100"
-            />
-            <button
-              type="submit" // form onSubmit={onSubmit}
-              className="absolute right-1 top-1 flex h-7 w-16 items-center justify-center rounded bg-neutral-200 px-2 py-1 font-medium text-neutral-900 dark:bg-neutral-700 dark:text-neutral-100"
-              disabled={!session?.user}
-            >
-              Sign
-            </button>
+        {session?.user ? (
+          <>
+            <div className="mx-auto my-2 w-full max-w-2xl rounded-md border border-gray-200 bg-white px-6 py-2 shadow-xl shadow-gray-400 dark:border-zinc-900 dark:bg-zinc-900 dark:shadow-none">
+              <div className="grid items-center justify-center">
+                <h1 className="mr-6 flex w-full flex-col items-center justify-center text-center text-xl font-bold tracking-tighter">
+                  sign my guestbook
+                </h1>
+              </div>
+              <div className="flex flex-col">
+                <form onSubmit={customHandleSubmit(onSubmit)} className="mb-2 flex flex-col items-center space-y-4">
+                  <label htmlFor="content" className="sr-only">
+                    {`Your Message`}
+                  </label>
+                  <textarea
+                    {...register('content', { required: true })}
+                    id="content"
+                    aria-label="Your message"
+                    placeholder="Your message..."
+                    required
+                    rows={3}
+                    maxLength={500}
+                    className="w-full rounded-md border border-gray-300 text-sm shadow-sm focus:border-gray-500 focus:ring-gray-500 dark:border-zinc-600 dark:bg-zinc-700 dark:text-white dark:focus:border-gray-700 dark:focus:ring-neutral-600"
+                  />
+
+                  <button
+                    type="submit"
+                    className="grid w-full place-items-center rounded bg-neutral-100 px-3 py-1 font-medium ring-gray-300 transition-all hover:ring-2 dark:bg-gray-600"
+                    disabled={!session?.user}
+                  >
+                    Sign
+                  </button>
+                </form>
+              </div>
+            </div>
+
             <SignOut />
-          </form>
-        </>
-      ) : (
-        <SignIn />
-      )}
+          </>
+        ) : (
+          <SignIn />
+        )}
+      </div>
     </>
   )
 }
