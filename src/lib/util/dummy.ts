e1e761bb96786e5c94c08f1cd26b3794a421b133
Thien Nguyen <nvktvan@gmail.com>
2023-08-24 13:16:56 -0700

    🔓 Clean up III: ..That was unesscary

---
+++ src/lib/util/dummy.ts
@@ -0,0 +1,364 @@
+  /**
+ * This could be made into a colection of data
+ * and store it in the db, but it would be harder
+ * to maintain and update. So I decided to just
+ * hard code it in here.
+ */
+export const FeaturedProjectItems = [
+  {
+    year    : '2023',
+    projects: [
+      {
+        title      : 'Todo? App',
+        description: 'Got me an internship offer ngl',
+        url        : 'https://github.com/Thienguen/TodoApp',
+        active     : true,
+        icon       : 'podcasts',
+        stats      : 'Yet another Todo App ♻️',
+        tech       : ['', ''],
+      },
+      {
+        title      : 'Gate Keeper',
+        description: 'QR Code based for event management, private event',
+        url        : 'Under Construction',
+        active     : true,
+        icon       : 'code',
+        stats      : 'Under 📑 Construction',
+        tech       : ['', ''],
+      },
+      {
+        title      : 'CLI Games',
+        description: 'Interact directly with the command line',
+        url        : 'https://github.com/Thienguen/Some-Command-Line-Interaction',
+        active     : true,
+        icon       : 'home',
+        stats      : 'C++, C#, Python, Js, Java, etc.',
+        tech       : ['', ''],
+      },
+      {
+        title      : 'Android Apps',
+        description: 'Simple Android apps. Fitbit, Movie App, etc.',
+        url        : 'https://github.com/Thienguen/CodePath-Intermediate-Android-Development',
+        active     : true,
+        icon       : 'projects',
+        stats      : 'From CodePath with luv ♡',
+        tech       : ['', ''],
+      },
+    ],
+  },
+]
+
+export const CareerItems = [
+  {
+    year    : '2023',
+    projects: [
+      {
+        title      : 'DecentrePX',
+        description: 'Software Engineer Intern focus on Web3, React, Solidity, etc.',
+        since      : '-- Since Dec 2022',
+        url        : 'Nay Nay',
+        active     : true,
+        icon       : 'clipboard',
+        stats      : 'Web3, React, Solidity, etc.',
+        tech       : ['Prisma', 'Nextjs', 'TypeScript', 'TailwindCSS', 'MongoDB.'],
+      },
+      {
+        title      : 'UNLV Tutor',
+        description: 'IT Tutor at the Academic Success Center II',
+        since      : '-- Since Jan 2023',
+        url        : 'Nay Nay',
+        active     : true,
+        icon       : 'clipboard',
+        stats      : '??',
+        tech       : ['Python', 'C++', 'C#'],
+      },
+      {
+        title      : 'UNLV Teacher Assistant',
+        description: 'Helping and grading students with their assignments II',
+        since      : 'Since May 2023 -- July 2023',
+        url        : 'Not Available',
+        active     : true,
+        icon       : 'heart',
+        stats      : 'Yet another day to help students Part 2',
+        tech       : ['C++'],
+      },
+    ],
+  },
+  {
+    year    : '2022',
+    projects: [
+      {
+        title      : 'CSN Teacher Assistant',
+        description: 'Helping and grading students with their assignments.',
+        since      : 'Since Aug 2022 -- Dec 2022',
+        url        : 'Not Available',
+        active     : true,
+        icon       : 'clipboard',
+        stats      : 'Yet another day to help students',
+        tech       : ['C++'],
+      },
+      {
+        title      : 'UNLV Tutor',
+        description: 'IT Tutor at the Academic Success Center.',
+        since      : 'Since Aug 2022 -- Dec 2022',
+        url        : 'Not Available',
+        active     : true,
+        icon       : 'home',
+        stats      : 'Yet another day to help students Part 3',
+        tech       : ['Python', 'C++', 'C#'],
+      },
+    ],
+  },
+  {
+    year    : '2021',
+    projects: [
+      {
+        title      : 'Started Coding',
+        description: "That one time the compiler didn't yell at me",
+        since      : '-- Since May 2021',
+        url        : 'https://github.com/Thienguen',
+        active     : true,
+        icon       : 'clipboard',
+        stats      : 'Tf do I do with this?',
+        tech       : ['C++', 'Bash'],
+      },
+    ],
+  },
+]
+
+export const ProjectItems = [
+  {
+    year    : '2023',
+    projects: [
+      {
+        title      : 'Portfolio',
+        description: 'The website you are currently viewing right now~',
+        url        : 'https://github.com/Thienguen/thien-personal-website',
+        active     : true,
+        icon       : 'code',
+        stats      : 'Next.js, TypeScript, TailwindCSS, etc.',
+        tech       : ['Nextjs', 'TypeScript', 'TailwindCSS', 'Prisma', 'etc.'],
+      },
+      {
+        title      : 'Gate Keeper',
+        description: 'QR Code based gate keeper',
+        url        : 'Nay Nay',
+        active     : true,
+        icon       : 'home',
+        stats      : 'Under 📑 Construction',
+        tech       : ['Nextjs', 'TailwindCSS', 'Prisma', 'TypeScript', 'etc.'],
+      },
+      {
+        title      : 'DSA Course',
+        description: 'Ah yes... A collection of DSA problems and solutions',
+        url        : 'https://github.com/Thienguen/Data-Structure-Algorithms-Course',
+        active     : true,
+        icon       : 'heart',
+        stats      : 'That one time my code make no sense to me',
+        tech       : ['C++', 'Python', 'Java', 'etc.'],
+      },
+    ],
+  },
+  {
+    year    : '2022',
+    projects: [
+      {
+        title      : 'Simple Android Apps',
+        description: 'Including: Fitbit, Movie App, etc.',
+        url        : 'https://github.com/Thienguen/Computer-Animations-Projects',
+        active     : true,
+        icon       : 'download',
+        stats      : 'Yet another day Jetbrains sold me their product',
+        tech       : ['HTTP', 'Kotlin', 'etc.'],
+      },
+      {
+        title      : 'Animation Projects',
+        description: 'Doing animation with SDL2 C++ Library',
+        url        : 'https://github.com/Thienguen/Computer-Animations-Projects',
+        active     : true,
+        icon       : 'download',
+        stats      : 'Yet another day Jetbrains sold me their product',
+        tech       : ['C++', 'SDL2', 'etc.'],
+      },
+      {
+        title      : 'DSA & Leetcode',
+        description: 'A collection of my personal DSA and Leetcode solutions',
+        url        : 'https://github.com/Thienguen/LeetCode-Data-Structure-n-Algorithms',
+        active     : true,
+        icon       : 'projects',
+        stats      : 'Yet another day to help students Part 2',
+        tech       : ['C++', 'Java', 'Python', 'etc.'],
+      },
+      {
+        title      : 'Todo? App',
+        description: `Keeping the data in Local Storage, won't lost after refresh, that was cool when I made it`,
+        url        : 'https://github.com/Thienguen/TodoApp',
+        active     : true,
+        icon       : 'podcasts',
+        stats      : 'Yet another Todo App ♻️',
+        tech       : ['React'],
+      },
+    ],
+  },
+  {
+    year    : '2021',
+    projects: [
+      {
+        title      : 'Introductory to C++',
+        description: 'C++ is not a bad langauge, but pointer...pointer scared me',
+        url        : 'https://github.com/Thienguen/Introductory-to-Computer-Science-Integrated',
+        active     : true,
+        icon       : 'clipboard',
+        stats      : 'Tf do I do with this?',
+        tech       : ['C++'],
+      },
+    ],
+  },
+]
+
+export const imageForGithub = {
+    /* jpg */
+  image1: '/images/arararara.jpg',
+  image2: '/images/bet.jpg',
+  image3: '/images/explosion.jpg',
+  image4: '/images/miku.jpg',
+  image5: '/images/monika3.jpg',
+    // image6: '/images/omori.jpg',
+  image7: '/images/ty.jpg',
+
+    /* gif */
+  image8 : '/images/duck-pixel.gif',
+  image9 : '/images/kafka.gif',
+  image10: '/images/chainsaw-man.gif',
+
+  image11: '/images/saber.jpg',
+  image12: '/images/anhbana.jpg',
+  image13: '/images/anhbana2.jpg',
+  image14: '/images/suynghi.jpg',
+  image15: '/images/tamangoaidao.jpg',
+  image16: '/images/justthe2ofus.jpg',
+  image17: '/images/justthe2ofus2.jpg',
+  image18: '/images/chainsaw.jpg',
+  image19: '/images/hanekawa.jpg',
+}
+
+export const RPGMakerItems = [
+  {
+    title  : 'Blank Dreams',
+    authors: 'Kanawo (Teriyaki Tomato)',
+    genres : 'Psychological, Horror',
+    image  : '/rpg/blank-dreams.jpg',
+  },
+  {
+    title  : 'Purgatory 2',
+    authors: 'Nama',
+    genres : 'Horror, Puzzle, Mystery, Adventure',
+    image  : '/rpg/purgatory2.png',
+  },
+  {
+    title  : 'Blight Dreams',
+    authors: 'Kanawo (Teriyaki Tomato)',
+    genres : 'Psychological, Horror',
+    image  : '/rpg/blight-dreams.png',
+  },
+  {
+    title  : 'Black Souls',
+    authors: 'Sushi Yuusha Toro',
+    genres : 'Horror, Action, Mystery',
+    image  : '/rpg/blacksouls.webp',
+  },
+  {
+    title  : 'One Shot',
+    authors: 'Eliza Velasquez, Nightmargin, GIRakaCheezer',
+    genres : 'Puzzle-adventure',
+    image  : '/rpg/oneshot.png',
+  },
+  {
+    title  : 'To the Moon',
+    authors: 'Freebird Games',
+    genres : 'Adventure Role-Playing',
+    image  : '/rpg/to-the-moon.jpg',
+  },
+  {
+    title  : 'Finding Paradise',
+    authors: 'Freebird Games',
+    genres : 'Adventure Role-Playing',
+    image  : '/rpg/finding-paradise.jpg',
+  },
+  {
+    title  : 'Paranormal Syndrome',
+    authors: 'Yuuyami no Kisetsu',
+    genres : 'Horror, Action, Mystery',
+    image  : '/rpg/paranormal-syndrome.webp',
+  },
+  {
+    title  : 'Charon',
+    authors: 'Everything about Charon',
+    genres : 'Psychological, Horror, Mystery, Puzzle',
+    image  : '/rpg/charon.webp',
+  },
+  {
+    title  : 'Ao Oni',
+    authors: 'Thien Nguyen',
+    genres : 'Survival, Horror',
+    image  : '/rpg/ao-oni.jpg',
+  },
+  {
+    title  : 'Pocket Mirror',
+    authors: 'AstralShift',
+    genres : 'Horror, Mystery, Adventure',
+    image  : '/rpg/pocket-mirror.png',
+  },
+  {
+    title  : 'Mad Father',
+    authors: 'Sen',
+    genres : 'Horror, Puzzle, Adventure',
+    image  : '/rpg/mad-father.gif',
+  },
+  {
+    title  : 'Misao',
+    authors: 'Sen',
+    genres : 'Horror, Puzzle, Adventure',
+    image  : '/rpg/misao.jpg',
+  },
+  {
+    title  : 'Ib',
+    authors: 'kouri',
+    genres : 'Horror, Puzzle',
+    image  : '/rpg/ib.webp',
+  },
+
+  {
+    title  : "The Witch's House",
+    authors: 'Fummy',
+    genres : 'Puzzle, Horror, Adventure',
+    image  : '/rpg/the-witch-house.jpg',
+  },
+  {
+    title  : 'Cat in the box',
+    authors: 'Gustav',
+    genres : 'Horror, Puzzle, Mystery',
+    image  : '/rpg/cat-in-the-box.webp',
+  },
+  {
+    title  : 'Purgatory',
+    authors: 'Nama',
+    genres : 'Horror, Puzzle, Mystery',
+    image  : '/rpg/purgatory.png',
+  },
+  {
+    title  : 'Omori',
+    authors: 'OMOCAT',
+    genres : 'Horror, Puzzle, Psychological',
+    image  : '/rpg/omori.gif',
+  },
+  {
+    title  : "There's more...",
+    authors: 'TBD',
+    genres : 'A lot more',
+    image  : '/images/anhbana.jpg',
+  },
+    // etc. 12:29:36
+]
+
+
