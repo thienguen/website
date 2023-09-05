3390b2645b8cf6f16a7c7d13b5c973e0ba252474
Thien Nguyen <nvktvan@gmail.com>
2023-09-04 17:51:13 -0700

    ✅ fixed: bug of branching II

--- src/components/about-me/index.ts
+++ src/components/about-me/index.ts
@@ -1,2 +1,2 @@
-export { default as Timer }          from '@/components/about-me/out'
+export { default as Timer }          from '@/components/about-me/timer'
 export { default as ContactMessage } from '@/components/about-me/contact-message'
