e65aece36b4c49a5ecb69cc6f403578f804ba0c9
Thien Nguyen <nvktvan@gmail.com>
2023-09-11 15:58:42 -0700

    ✂️ ui: see if the bar was annoyned

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,17 +7,17 @@ import HomeHero from '@/components/home/home-hero'
 // import Profile from '@/components/home/profile'
 // import SkillIssues from '@/components/home/skill-issues'
 
-const BarLeft = () => {
-  return (
-    <div className="my-6 hidden h-[3px] w-screen animate-fade-left bg-gradient-to-r from-black via-gray-700/50 to-gray-700/0 dark:bg-gradient-to-r dark:from-white dark:via-zinc-300/50 dark:to-zinc-300/0 md:block" />
-  )
-}
+// const BarLeft = () => {
+//   return (
+//     <div className="my-6 hidden h-[3px] w-screen animate-fade-left bg-gradient-to-r from-black via-gray-700/50 to-gray-700/0 dark:bg-gradient-to-r dark:from-white dark:via-zinc-300/50 dark:to-zinc-300/0 md:block" />
+//   )
+// }
 
-const BarRight = () => {
-  return (
-    <div className="my-6 hidden h-[3px] w-screen animate-fade-right bg-gradient-to-r from-gray-700/0 via-gray-700/50 to-black dark:bg-gradient-to-r dark:from-zinc-300/0 dark:via-zinc-300/50 dark:to-white md:block" />
-  )
-}
+// const BarRight = () => {
+//   return (
+//     <div className="my-6 hidden h-[3px] w-screen animate-fade-right bg-gradient-to-r from-gray-700/0 via-gray-700/50 to-black dark:bg-gradient-to-r dark:from-zinc-300/0 dark:via-zinc-300/50 dark:to-white md:block" />
+//   )
+// }
 
 /**
  * Landing page, not the prettiest, and
@@ -47,9 +47,9 @@ export default function Home() {
       {/* Hero? Section */}
       {/* Bar */}
       {/* ------------------------------------------------------------------------------------------------- */}
-      <BarLeft />
+      {/* <BarLeft /> */}
       <HomeHero />
-      <BarRight />
+      {/* <BarRight /> */}
       {/* ------------------------------------------------------------------------------------------------- */}
       {/* Bar */}
     </>
