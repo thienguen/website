a95068175242d09279071d070be295ef02377da1
Thien Nguyen <nvktvan@gmail.com>
2023-09-13 05:08:50 -0700

    💩 huh: ??

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,7 @@
   // eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path')
 
-  /** @type {import("eslint").Linter.Config} */
+/** @type {import("eslint").Linter.Config} */
 const config = {
   overrides: [
     {
--- src/lib/util/font.ts
+++
@@ -1,6 +0,0 @@
-// import { JetBrains_Mono as FontMono } from "next/font/google"
-
-// export const fontMono = FontMono({
-//   subsets: ["latin"],
-//   variable: "--font-mono",
-// })
